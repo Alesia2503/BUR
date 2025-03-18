309a2e30fe0667ee12d96af3109c5b13879c8818
Alesia_Shikurova <ales2503@yandex.by>
2025-03-18 20:44:57 +0300

    ftat: style

--- script.js
+++ script.js
@@ -23,15 +23,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 // Smooth Scroll
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function(e) {
-      e.preventDefault();
-      const target = document.querySelector(this.getAttribute('href'));
-      if (target) {
-          target.scrollIntoView({
-              behavior: 'smooth',
-              block: 'start'
-          });
-      }
-  });
-});
+// document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+//   anchor.addEventListener('click', function(e) {
+//       e.preventDefault();
+//       const target = document.querySelector(this.getAttribute('href'));
+//       if (target) {
+//           target.scrollIntoView({
+//               behavior: 'smooth',
+//               block: 'start'
+//           });
+//       }
+//   });
+// });
